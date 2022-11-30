3e2ccf0620bfb6a42b4fb0bf772ad3c4fc016956
smok313-zdr <traxer@op.pl>
2022-11-30 11:37:57 +0100

    mysql database connection

--- app.R
+++ app.R
@@ -1,12 +1,29 @@
 library(shiny)
+library(RMySQL)
 
+# Mysql database connection
+mysqlconnection = dbConnect(RMySQL::MySQL(),
+                            dbname='cars',
+                            host='localhost',
+                            port=3306,
+                            user='Kacper',
+                            password='Passw0rd')
 
+brand = fetch(dbSendQuery(mysqlconnection, "select brand from sale_2018"))
+model = fetch(dbSendQuery(mysqlconnection, "select model from sale_2018"))
+sold = fetch(dbSendQuery(mysqlconnection, "select amountSold from sale_2018"))
+
+# Data preparation
+dfall <- data.frame(brand,model,sold)
+colnames(dfall) <- c("Brand","Model","Amount sold")
+
+# UI layout -------------------------------------------------
 ui <- fluidPage(
 
     
 )
 
-
+# Server function -------------------------------------------
 server <- function(input, output) {
 
     
